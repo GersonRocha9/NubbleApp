c190fc19f3d930156f1ad6b382125878f53d06e8
Gerson Rocha <gersonrocha9@gmail.com>
2023-06-06 10:41:45 -0300

    feat: add font families on android and ios

--- App.tsx
+++ App.tsx
@@ -5,8 +5,20 @@ import {Text} from './src/components/Text/Text';
 export default function App() {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge">Heading Large</Text>
-      <Text preset="paragraphLarge">Paragraph Large</Text>
+      <Text
+        preset="headingLarge"
+        style={{
+          fontFamily: 'Satoshi Italic',
+        }}>
+        Heading Large
+      </Text>
+      <Text
+        preset="paragraphLarge"
+        style={{
+          fontFamily: 'Satoshi Bold',
+        }}>
+        Paragraph Large
+      </Text>
       <Text preset="paragraphSmall">Paragraph Small</Text>
       <Text preset="paragraphCaption">Paragraph Caption</Text>
     </SafeAreaView>
