d00bb95de50b8d6690413a1c05b7841ebeb9dc88
Gerson Rocha <gersonrocha9@gmail.com>
2023-06-08 16:23:36 -0300

    refactor: adjust padding on keyboard avoiding on Screen component

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,33 +1,55 @@
+import {KeyboardAvoidingView, Platform} from 'react-native';
 import React, {ReactNode} from 'react';
+import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer';
 
 import {Box} from '../Box/Box';
 import {Icon} from '../Icon/Icon';
 import {Text} from '../Text/Text';
 import {useAppSafeArea} from '../../hooks/useAppSafeArea';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 interface ScreenProps {
   children: ReactNode;
   canGoBack?: boolean;
+  scrollable?: boolean;
 }
 
-export function Screen({children, canGoBack}: ScreenProps) {
-  const {top} = useAppSafeArea();
+export function Screen({
+  children,
+  canGoBack = false,
+  scrollable = false,
+}: ScreenProps) {
+  const {top, bottom} = useAppSafeArea();
+  const {colors} = useAppTheme();
+
+  const Container = scrollable ? ScrollViewContainer : ViewContainer;
 
   return (
-    <Box
-      paddingHorizontal="s24"
+    <KeyboardAvoidingView
+      // eslint-disable-next-line react-native/no-inline-styles
       style={{
-        paddingTop: top,
-      }}>
-      {canGoBack && (
-        <Box flexDirection="row" gap="s8" alignItems="center" mb="s24">
-          <Icon name="arrowLeft" size={20} color="primary" />
-          <Text preset="paragraphMedium" semiBold>
-            Voltar
-          </Text>
+        flex: 1,
+      }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Container backgroundColor={colors.background}>
+        <Box
+          paddingHorizontal="s24"
+          paddingBottom="s24"
+          style={{
+            paddingTop: top,
+            paddingBottom: bottom,
+          }}>
+          {canGoBack && (
+            <Box flexDirection="row" gap="s8" alignItems="center" mb="s24">
+              <Icon name="arrowLeft" size={20} color="primary" />
+              <Text preset="paragraphMedium" semiBold>
+                Voltar
+              </Text>
+            </Box>
+          )}
+          {children}
         </Box>
-      )}
-      {children}
-    </Box>
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
---
+++ src/components/Screen/components/ScreenContainer.tsx
@@ -0,0 +1,29 @@
+import React, {ReactNode} from 'react';
+import {ScrollView, View} from 'react-native';
+
+interface Props {
+  children: ReactNode;
+  backgroundColor: string;
+}
+
+export function ScrollViewContainer({children, backgroundColor}: Props) {
+  return (
+    <ScrollView
+      style={{
+        backgroundColor,
+      }}>
+      {children}
+    </ScrollView>
+  );
+}
+
+export function ViewContainer({children, backgroundColor}: Props) {
+  return (
+    <View
+      style={{
+        backgroundColor,
+      }}>
+      {children}
+    </View>
+  );
+}
