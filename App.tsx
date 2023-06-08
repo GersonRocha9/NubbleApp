fc1cdca0bae965f3d08899125c01473cdab5cd67
Gerson Rocha <gersonrocha9@gmail.com>
2023-06-08 15:21:20 -0300

    refactor: creating LoginScreen file

--- App.tsx
+++ App.tsx
@@ -1,56 +1,12 @@
-import {Box} from './src/components/Box/Box';
-import {Button} from './src/components/Button/Button';
-import {Icon} from './src/components/Icon/Icon';
+import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {Text} from './src/components/Text/Text';
-import {TextInput} from './src/components/TextInput/TextInput';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <Box paddingHorizontal="s24">
-          <Text mb="s8" preset="headingLarge">
-            Olá
-          </Text>
-
-          <Text mb="s40" preset="paragraphLarge">
-            Digite seu e-mail e senha para entrar
-          </Text>
-
-          <TextInput
-            label="E-mail"
-            placeholder="Digite seu e-mail"
-            keyboardType="email-address"
-            errorMessage="E-mail inválido"
-            boxProps={{
-              mb: 's16',
-            }}
-          />
-
-          <TextInput
-            label="Senha"
-            placeholder="Digite sua senha"
-            textContentType="password"
-            secureTextEntry
-            RightComponent={<Icon name="eyeOn" size={24} color="gray2" />}
-            boxProps={{
-              mb: 's8',
-            }}
-          />
-
-          <Text preset="paragraphSmall" bold color="primary" mb="s48">
-            Esqueci minha senha
-          </Text>
-
-          <Button title="Entrar" mb="s12" />
-
-          <Button title="Criar uma conta" variant="outline" />
-        </Box>
-      </SafeAreaView>
+      <LoginScreen />
     </ThemeProvider>
   );
 }
