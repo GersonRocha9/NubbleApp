79f65307675940e036af0b113c16846b1d44faaa
Gerson Rocha <gersonrocha9@gmail.com>
2023-11-27 13:23:19 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text} from 'react-native';
+
+import React from 'react';
+
+export function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Test</Text>
+    </SafeAreaView>
+  );
+}
